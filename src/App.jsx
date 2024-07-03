9345c8df51ffd7892a84697329086ef5392c3e22
Mathias LEROY <mathias.leroy@ecoles-epsi.fr>
2024-07-03 15:07:17 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import 'aframe';
+import './App.css';
+import universe from  "../public/scene_stormtrooper.glb";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+function App() {
+    const loader = new GLTFLoader();
+    loader.load(universe, (d) => {
+        const entity = document.getElementById("universe");
+        entity.object3D.add(d.scene);
+    })
+    return (
+        <a-scene>
+            <a-assets>
+                <img id="sky" src={universe} alt="" />
+            </a-assets>
+            <a-entity id="universe" position="0 0 0" scale="0.1 0.1 0.1"></a-entity>
+            <a-sky color="FFFFFF" material="src:#sky" rotation="0 0 0"></a-sky>
+        </a-scene>
+    );
+}
+
+export default App;
